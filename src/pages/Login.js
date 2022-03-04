67b5277b5428a4b531523beeaf5546622a14521b
KerliS9 <kerlischroeder9@gmail.com>
2022-03-04 16:18:57 -0300

    build: iniciando o componente login :construction:

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import './Login.css';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="content">
+        <h1>TRYBE</h1>
+        <form className="form-login">
+          <input
+            className="input"
+            data-testid="email-input"
+            type="email"
+            placeholder="Digite seu email"
+          />
+          <input
+            className="input"
+            data-testid="password-input"
+            type="password"
+            placeholder="Digite sua senha"
+            minLength="6"
+          />
+          <button type="button">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
